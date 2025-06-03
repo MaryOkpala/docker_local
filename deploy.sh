82b2f3e38a0fba1960af9bb34de26290a0bda3f7
MaryOkpala <165200778+MaryOkpala@users.noreply.github.com>
2025-06-02 21:54:13 -0230

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,23 @@
+#!/bin/bash
+
+echo "deploying webapp containers..."
 docker pull maryokpala/image1:latest1
-docker stop acada-webapp1 acada-webapp2 acada-webapp3 acada-webapp4 acada-webapp5 || true
-docker rm acada-webapp1  acada-webapp2 acada-webapp3 acada-webapp4 acada-webapp5 -f
+
+echo "creating network..."
 docker network create acada-apps
-docker run -d --name acada-webapp1 --hostname acada-webapp1 --network acada-apps maryokpala/image1:latest1
-docker run -d --name acada-webapp2 --hostname acada-webapp2 --network acada-apps maryokpala/image1:latest1
-docker run -d --name acada-webapp3 --hostname acada-webapp3 --network acada-apps maryokpala/image1:latest1
-docker run -d --name acada-webapp4 --hostname acada-webapp4 --network acada-apps maryokpala/image1:latest1
-docker run -d --name acada-webapp5 --hostname acada-webapp5 --network acada-apps maryokpala/image1:latest1
+
+for i in {1..6};
+do
+
+docker stop acada-webapp$i ; docker rm -f acada-webapp$i || true
+docker run -d --name acada-webapp$i --hostname acada-webapp$i --network acada-apps maryokpala/image1:latest1;
+done
+echo "deploying webapp containers done..."
+
+sleep 20
+
+echo "deploying HAproxy containers..."
+docker rm haproxy -f >/dev/null 2>&1 || true
+docker run -d --name haproxy --network acada-apps -v /opt/docker_config_files/haproxy.cfg:/usr/local/etc/haproxy/haproxy.cfg:ro -p 9090:80 haproxy:latest
+docker ps | grep -i haproxy*
+echo "deploying HAproxy containers done..."
